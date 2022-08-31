0a1f0496fff191711607fdd658d32f1f16cfe25d
Helmi Amirudin <helmilaw@gmail.com>
2022-08-31 13:10:12 +0700

    gtwgtg

--- assets/js/prebid-ads.js
+++ assets/js/prebid-ads.js
@@ -2,5 +2,7 @@
 // https://stackoverflow.com/questions/4869154/how-to-detect-adblock-on-my-website?page=1&tab=scoredesc#tab-top
 
 function FuncHelmiAmirudinRunPrebidAds() {
-	let VarHelmiAmirudinRunPrebidAds = true;
+	var VarHelmiAmirudinRunPrebidAds = true;
 }
+
+function detectAdblock(){const adblockTests={uBlockOrigin:{url:"https://incolumitas.com/data/pp34.js?sv=",id:"837jlaBksSjd9jh"},adblockPlus:{url:"https://incolumitas.com/data/neutral.js?&ad_height=",id:"hfuBadsf3hFAk"}};function canLoadRemoteScript(obj){return new Promise(function(resolve,reject){var script=document.createElement("script");script.onload=function(){if(document.getElementById(obj.id)){resolve(false)}else{resolve(true)}};script.onerror=function(){resolve(true)};script.src=obj.url;document.body.appendChild(script)})}return new Promise(function(resolve,reject){let promises=[canLoadRemoteScript(adblockTests.uBlockOrigin),canLoadRemoteScript(adblockTests.adblockPlus)];Promise.all(promises).then(results=>{resolve({uBlockOrigin:results[0],adblockPlus:results[1]})}).catch(err=>{reject(err)})})}
